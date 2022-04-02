b1ee00d8597df22141a4e6e120e55b1a52306a71
Coleton Watt <electricninja102@gmail.com>
2022-04-02 11:42:58 -0600

    Initial Commit

---
+++ Lab: Solving Missionaries Cannibals 11.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Solving Missionaries Cannibals 11.4
+//
+//  Created by Coleton Watt on 4/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
